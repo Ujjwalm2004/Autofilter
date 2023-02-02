7c94f6180721c3311874cf657f4e86a9c2800930
Royaldeep1 <120001510+Royaldeep1@users.noreply.github.com>
2023-02-02 21:35:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -133,7 +133,6 @@ async def message_handler(event):
             author=Config.BOT_USERNAME
         )
         message = f'**Click Here 👇 For "{event.text}"**\n\n[🍿🎬 {str(event.text).upper()}\n🍿🎬 {str("Click me for results").upper()}]({tgraph_result})'
-        newbutton = [Button.url('Join Updates Channel ✅',f'https://t.me/sources_cods')]
 
         await txt.delete()
         result = await event.reply(message, link_preview=False)
